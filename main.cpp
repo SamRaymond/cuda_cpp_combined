246ed98cab1dadafbba988949f3a85e725483b6c
SamRaymond <sjr@mit.edu>
2019-12-12 15:38:37 -0500

    separated CUDA into cu/h files with a main to run both

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include <simpleAddCPU.hpp>
+#include <simpleAddGPU.h>
+
+
+int main(void){
+
+    long N = 100*1000*1000;
+    // int *a;
+    // a = new int[N];
+    // int *b;
+    // b = new int[N];
+    // int *c;
+    // c = new int[N];
+
+    double* a;
+    cudaMallocManaged(&a, N*sizeof(double));
+    double* b;
+    cudaMallocManaged(&b, N*sizeof(double));
+    double* c;
+    cudaMallocManaged(&c, N*sizeof(double));
+    
+    int blockSize = 256;
+    int numBlocks = (N + blockSize - 1) / blockSize;
+    randomize_cuda<<<1, blockSize>>>(a,b,c,N);
+    cudaDeviceSynchronize();
+    add_cuda<<<numBlocks, blockSize>>>(a,b,c,N);
+    cudaDeviceSynchronize();
+    for(int p=0;p<10;p++){
+        printf("%f, %f,%f \n",a[p],b[p],c[p]);
+    }
+    add_CPU(a,b,c,N);
+    for(int p=0;p<10;p++){
+        printf("%f, %f,%f \n",a[p],b[p],c[p]);
+    }
+    // delete a;
+    // delete b;
+    // delete c;
+    cudaDeviceSynchronize();
+    cudaFree(a);
+    cudaFree(b);
+    cudaFree(c);
+    return 0;
+}
