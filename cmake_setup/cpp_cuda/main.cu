ab902a654e949f111b59b75935dcacdbf31c0250
SamRaymond <sjr@mit.edu>
2019-12-17 09:58:48 -0500

    tested on PC with ccmake

--- cmake_setup/cpp_cuda/main.cu
+++ cmake_setup/cpp_cuda/main.cu
@@ -4,7 +4,7 @@
 
 int main(void){
 
-    long N = 300*100*1000; //~300 variables in Graphyt
+    long N = 300*1000*1000; //~300 variables in Graphyt
     // int *a;
     // a = new int[N];
     // int *b;
@@ -27,9 +27,12 @@ int main(void){
     add_cuda<<<numBlocks, blockSize>>>(a,b,c,N);
     cudaDeviceSynchronize();
     double t2 = omp_get_wtime();
+    
     for(int p=0;p<10;p++){
         printf("%f, %f,%f \n",a[p],b[p],c[p]);
     }
+    printf("Particles: %li\nGPU Time: %.2fs\n",N/300,t2-t1);
+
     double t3 = omp_get_wtime();
     add_CPU(a,b,c,N);
     double t4 = omp_get_wtime();
